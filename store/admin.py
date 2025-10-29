c8d1d8b1c34513d32a8b353f4392bf5c8dd32a3b
yinchi-34 <11894044+yinchi-34@user.noreply.gitee.com>
2025-10-29 11:04:48 +0800

    删除活跃用户字段相关操作

--- store/admin.py
+++ store/admin.py
@@ -5,10 +5,9 @@ from store.models import Order, Customer
 # Register your models here.
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'email', 'phone', 'membership', 'is_active']
+    list_display = ['first_name', 'last_name', 'email', 'phone', 'membership',]
     list_editable = ('membership',)
     list_per_page = 10
-    list_filter = ('user__is_active',)
     list_select_related = ['user']
     search_fields = ['first_name__istartswith', 'user__last_name__istartswith']
     ordering = ('user__first_name', 'user__last_name')
@@ -24,10 +23,6 @@ class CustomerAdmin(admin.ModelAdmin):
     @admin.display(ordering='user__email',)
     def email(self, customer):
         return customer.user.email
-
-    @admin.display(ordering='user__is_active',)
-    def is_active(self, customer):
-        return customer.user.is_active
 
 
 class OrderAdmin(admin.ModelAdmin):
